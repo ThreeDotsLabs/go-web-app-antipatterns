282099adb7faae501873931fa1664dce39151162
Michael Samoylov <michael.samoylov@gmail.com>
2021-10-12 16:44:44 +0300

    Add more idiomatic Go implementation

--- 02-enums/04-structs/role/role.go
+++ 02-enums/04-structs/role/role.go
@@ -1,6 +1,17 @@
 package role
+import (
+	"errors"
+	"fmt"
+)
 
-import "errors"
+var (
+	Guest     = Role{"guest"}
+	Member    = Role{"member"}
+	Moderator = Role{"moderator"}
+	Admin     = Role{"admin"}
+
+	ErrUnknownRole = errors.New("unknown role")
+)
 
 type Role struct {
 	slug string
@@ -10,25 +21,17 @@ func (r Role) String() string {
 	return r.slug
 }
 
-var (
-	Unknown   = Role{""}
-	Guest     = Role{"guest"}
-	Member    = Role{"member"}
-	Moderator = Role{"moderator"}
-	Admin     = Role{"admin"}
-)
-
-func FromString(s string) (Role, error) {
+func FromString(s string) (*Role, error) {
 	switch s {
 	case Guest.slug:
-		return Guest, nil
+		return &Guest, nil
 	case Member.slug:
-		return Member, nil
+		return &Member, nil
 	case Moderator.slug:
-		return Moderator, nil
+		return &Moderator, nil
 	case Admin.slug:
-		return Admin, nil
+		return &Admin, nil
+	default:
+		return nil, fmt.Errorf("%w: %s", ErrUnknownRole, s)
 	}
-
-	return Unknown, errors.New("unknown role: " + s)
 }
