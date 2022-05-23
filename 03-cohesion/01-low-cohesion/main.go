af27831e66a3c667d55faa7da9a274174cf712d5
Miłosz Smółka <milosz.smolka@gmail.com>
2022-05-23 21:10:23 +0200

    Update main

--- 03-cohesion/01-low-cohesion/main.go
+++ 03-cohesion/01-low-cohesion/main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"log"
+	"net/http"
 	"os"
 )
 
@@ -10,20 +12,103 @@ func main() {
 	logger := log.New(os.Stdout, "", log.LstdFlags)
 	handler := NewSubscribeHandler(logger, nopMetricsClient{})
 
-	cmd := Subscribe{
-		Email:        "user@example.com",
-		NewsletterID: "product-news",
+	httpHandler := func(w http.ResponseWriter, r *http.Request) {
+		var request SubscribeHTTPRequest
+		err := json.NewDecoder(r.Body).Decode(&request)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		cmd := Subscribe{
+			Email:        request.Email,
+			NewsletterID: request.NewsletterID,
+		}
+
+		user, err := userFromRequest(r)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		ctx := ContextWithUser(r.Context(), user)
+
+		err = handler.Execute(ctx, cmd)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
 	}
 
-	ctx := ContextWithUser(context.Background(), User{
+	eventHandler := func(ctx context.Context, event UserSignedUp) error {
+		if !event.ProductNewsConsent {
+			return nil
+		}
+
+		fakeUser := User{
+			ID:     event.ID,
+			Active: true,
+		}
+
+		ctx = ContextWithUser(ctx, fakeUser)
+
+		cmd := Subscribe{
+			Email:        event.Email,
+			NewsletterID: "product-news",
+		}
+
+		return handler.Execute(ctx, cmd)
+	}
+
+	rpcHandler := func(ctx context.Context, req SubscribeRPCRequest) error {
+		fakeUser := User{
+			ID:     "1", // Missing ID in the context, let's assume it's the root user making changes
+			Active: true,
+		}
+
+		ctx = ContextWithUser(ctx, fakeUser)
+
+		cmd := Subscribe{
+			Email:        req.Email,
+			NewsletterID: "product-news",
+		}
+
+		return handler.Execute(ctx, cmd)
+	}
+
+	_ = httpHandler
+	_ = eventHandler
+	_ = rpcHandler
+}
+
+func userFromRequest(r *http.Request) (User, error) {
+	token := r.Header.Get("Authorization")
+
+	// Verify the token, create the user struct out of it
+	_ = token
+
+	user := User{
 		ID:     "1000",
 		Active: true,
-	})
-
-	err := handler.Execute(ctx, cmd)
-	if err != nil {
-		log.Fatal(err)
 	}
+
+	return user, nil
+}
+
+type SubscribeHTTPRequest struct {
+	Email        string `json:"email"`
+	NewsletterID string `json:"newsletter_id"`
+}
+
+type SubscribeRPCRequest struct {
+	Email        string
+	NewsletterID string
+}
+
+type UserSignedUp struct {
+	ID                 string
+	Email              string
+	ProductNewsConsent bool
 }
 
 type nopMetricsClient struct{}
--- 03-cohesion/03-generics/main.go
+++ 03-cohesion/03-generics/main.go
@@ -2,40 +2,102 @@ package main
 
 import (
 	"context"
-	"fmt"
+	"encoding/json"
 	"log"
+	"net/http"
 	"os"
 )
 
 func main() {
 	logger := log.New(os.Stdout, "", log.LstdFlags)
-	handler := NewAuthorizedSubscribeHandler(logger, nopMetricsClient{})
-
+	authorizedHandler := NewAuthorizedSubscribeHandler(logger, nopMetricsClient{})
 	unauthorizedHandler := NewUnauthorizedSubscribeHandler(logger, nopMetricsClient{})
 
-	cmd := Subscribe{
-		Email:        "user@example.com",
-		NewsletterID: "product-news",
+	httpHandler := func(w http.ResponseWriter, r *http.Request) {
+		var request SubscribeHTTPRequest
+		err := json.NewDecoder(r.Body).Decode(&request)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		cmd := Subscribe{
+			Email:        request.Email,
+			NewsletterID: request.NewsletterID,
+		}
+
+		user, err := userFromRequest(r)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		ctx := ContextWithUser(r.Context(), user)
+
+		err = authorizedHandler.Execute(ctx, cmd)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
 	}
 
-	ctx := ContextWithUser(context.Background(), User{
+	eventHandler := func(ctx context.Context, event UserSignedUp) error {
+		if !event.ProductNewsConsent {
+			return nil
+		}
+
+		cmd := Subscribe{
+			Email:        event.Email,
+			NewsletterID: "product-news",
+		}
+
+		return unauthorizedHandler.Execute(ctx, cmd)
+	}
+
+	rpcHandler := func(ctx context.Context, req SubscribeRPCRequest) error {
+		cmd := Subscribe{
+			Email:        req.Email,
+			NewsletterID: "product-news",
+		}
+
+		return unauthorizedHandler.Execute(ctx, cmd)
+	}
+
+	_ = httpHandler
+	_ = eventHandler
+	_ = rpcHandler
+}
+
+func userFromRequest(r *http.Request) (User, error) {
+	token := r.Header.Get("Authorization")
+
+	// Verify the token, create the user struct out of it
+	_ = token
+
+	user := User{
 		ID:     "1000",
 		Active: true,
-	})
-
-	err := handler.Execute(ctx, cmd)
-	if err != nil {
-		log.Fatal(err)
 	}
 
-	err = unauthorizedHandler.Execute(context.Background(), cmd)
-	if err != nil {
-		log.Fatal(err)
-	}
+	return user, nil
+}
+
+type SubscribeHTTPRequest struct {
+	Email        string `json:"email"`
+	NewsletterID string `json:"newsletter_id"`
+}
+
+type SubscribeRPCRequest struct {
+	Email        string
+	NewsletterID string
+}
+
+type UserSignedUp struct {
+	ID                 string
+	Email              string
+	ProductNewsConsent bool
 }
 
 type nopMetricsClient struct{}
 
-func (c nopMetricsClient) Inc(key string, value int) {
-	fmt.Println("Incrementing key", key, "by", value)
-}
+func (c nopMetricsClient) Inc(key string, value int) {}
