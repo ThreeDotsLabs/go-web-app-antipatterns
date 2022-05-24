835b211e2ce628092ee4dbc3545e39b5047ee5f2
Miłosz Smółka <milosz.smolka@gmail.com>
2022-05-24 22:20:13 +0200

    Move files around

--- 03-cohesion/02-decorators/subscribe.go
+++ 03-cohesion/02-decorators/subscribe.go
@@ -2,9 +2,6 @@ package main
 
 import (
 	"context"
-	"errors"
-	"log"
-	"time"
 )
 
 type MetricsClient interface {
@@ -55,60 +52,4 @@ func NewSubscribeHandler() SubscribeHandler {
 func (h subscribeHandler) Execute(ctx context.Context, cmd Subscribe) error {
 	// Subscribe the user to the newsletter
 	return nil
-}
-
-type subscribeLoggingDecorator struct {
-	base   SubscribeHandler
-	logger Logger
-}
-
-func (d subscribeLoggingDecorator) Execute(ctx context.Context, cmd Subscribe) (err error) {
-	d.logger.Println("Subscribing to newsletter", cmd)
-	defer func() {
-		if err == nil {
-			log.Println("Subscribed to newsletter")
-		} else {
-			log.Println("Failed subscribing to newsletter:", err)
-		}
-	}()
-
-	return d.base.Execute(ctx, cmd)
-}
-
-type subscribeMetricsDecorator struct {
-	base   SubscribeHandler
-	client MetricsClient
-}
-
-func (d subscribeMetricsDecorator) Execute(ctx context.Context, cmd Subscribe) (err error) {
-	start := time.Now()
-	defer func() {
-		end := time.Now().Sub(start)
-		d.client.Inc("commands.subscribe.duration", int(end.Seconds()))
-
-		if err == nil {
-			d.client.Inc("commands.subscribe.success", 1)
-		} else {
-			d.client.Inc("commands.subscribe.failure", 1)
-		}
-	}()
-
-	return d.base.Execute(ctx, cmd)
-}
-
-type subscribeAuthorizationDecorator struct {
-	base SubscribeHandler
-}
-
-func (d subscribeAuthorizationDecorator) Execute(ctx context.Context, cmd Subscribe) error {
-	user, err := UserFromContext(ctx)
-	if err != nil {
-		return err
-	}
-
-	if !user.Active {
-		return errors.New("the user's account is not active")
-	}
-
-	return d.base.Execute(ctx, cmd)
 }
--- 03-cohesion/03-generics/subscribe.go
+++ 03-cohesion/03-generics/subscribe.go
@@ -2,11 +2,6 @@ package main
 
 import (
 	"context"
-	"errors"
-	"fmt"
-	"log"
-	"strings"
-	"time"
 )
 
 type CommandHandler[C any] interface {
@@ -57,64 +52,4 @@ func NewSubscribeHandler() CommandHandler[Subscribe] {
 func (H SubscribeHandler) Execute(ctx context.Context, cmd Subscribe) error {
 	// Subscribe the user to the newsletter
 	return nil
-}
-
-type loggingDecorator[C any] struct {
-	base   CommandHandler[C]
-	logger Logger
-}
-
-func (d loggingDecorator[C]) Execute(ctx context.Context, cmd C) (err error) {
-	d.logger.Println("Executing command", commandName(cmd), cmd)
-	defer func() {
-		if err == nil {
-			log.Println("Command executed successfully")
-		} else {
-			log.Println("Failed to execute command:", err)
-		}
-	}()
-
-	return d.base.Execute(ctx, cmd)
-}
-
-type metricsDecorator[C any] struct {
-	base   CommandHandler[C]
-	client MetricsClient
-}
-
-func (d metricsDecorator[C]) Execute(ctx context.Context, cmd C) (err error) {
-	start := time.Now()
-	defer func() {
-		end := time.Now().Sub(start)
-		d.client.Inc(fmt.Sprintf("commands.%s.duration", commandName(cmd)), int(end.Seconds()))
-
-		if err == nil {
-			d.client.Inc(fmt.Sprintf("commands.%s.success", commandName(cmd)), 1)
-		} else {
-			d.client.Inc(fmt.Sprintf("commands.%s.failure", commandName(cmd)), 1)
-		}
-	}()
-
-	return d.base.Execute(ctx, cmd)
-}
-
-type authorizationDecorator[C any] struct {
-	base CommandHandler[C]
-}
-
-func (d authorizationDecorator[C]) Execute(ctx context.Context, cmd C) error {
-	user, err := UserFromContext(ctx)
-	if err != nil {
-		return err
-	}
-
-	if !user.Active {
-		return errors.New("the user's account is not active")
-	}
-
-	return d.base.Execute(ctx, cmd)
-}
-
-func commandName(cmd any) string {
-	return strings.ToLower(strings.Split(fmt.Sprintf("%T", cmd), ".")[1])
 }
